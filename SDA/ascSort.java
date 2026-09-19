06f628657fcfd95478f9a56d12e91a48ab959a22
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-19 11:57:34 +0530

    Fixed comments

--- SDA/ascSort.java
+++ SDA/ascSort.java
@@ -20,8 +20,8 @@ public class ascSort {
 
     // bubble sort method
     void sort() {
-        for (i = 0; i < n - 1; i++) { // runs from 0 to second last element of the array
-            for (j = 0; j < n - i - 1; j++) { // runs from 0 to third last element of the array
+        for (i = 0; i < n - 1; i++) { // runs from 0 to last element of the array
+            for (j = 0; j < n - i - 1; j++) { // runs from 0 to second last element of the array
                 if (arr[j] > arr[j + 1]) { // if first element is greater that the second, swap!
                     t = arr[j];
                     arr[j] = arr[j + 1];
