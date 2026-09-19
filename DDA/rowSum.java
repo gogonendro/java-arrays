b71db1d9e0cb9e09a3109e56e9403ce8a954cc83
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-19 20:57:49 +0530

    Add program to find the sum of elements in each row

---
+++ DDA/rowSum.java
@@ -0,0 +1,49 @@
+package DDA;
+
+import java.util.*;
+
+public class rowSum {
+    Scanner sc = new Scanner(System.in);
+    int n, m, i, j, s, arr[][];
+
+    void acc() {
+        System.out.println("Enter the number of rows and columns:");
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n][m];
+
+        System.out.println("Enter the elements of the " + n + "x" + m + " matrix:");
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    void calc_dis() {
+        System.out.println("Matrix:");
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        for (i = 0; i < n; i++) {
+            s = 0; // reset sum before starting new row
+
+            for (j = 0; j < m; j++) {
+                s = s + arr[i][j]; // add every element in the current row
+            }
+            System.out.println("Sum of elements in row " + (i + 1) + " = " + s);
+        }
+    }
+
+    public static void main(String[] args) {
+        rowSum obj = new rowSum();
+        obj.acc();
+        obj.calc_dis();
+        obj.sc.close();
+    }
+}
