47bd7d8dd2eacdbb00122b69719b33ed9fbed972
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-19 21:49:18 +0530

    Add program to find the difference of two matrices

---
+++ DDA/matDiff.java
@@ -0,0 +1,74 @@
+package DDA;
+
+import java.util.*;
+
+public class matDiff {
+    Scanner sc = new Scanner(System.in);
+    int n, m, i, j, arr1[][], arr2[][], arr3[][];
+
+    void acc() {
+        System.out.println("Enter the number of rows and columns of matrices:");
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr1 = new int[n][m];
+        arr2 = new int[n][m];
+
+        System.out.println("Enter the elements of the " + n + "x" + m + " matrix 1:");
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter the elements of the " + n + "x" + m + " matrix 2:");
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        arr3 = new int[n][m];
+    }
+
+    void calc_dis() {
+        System.out.println("Matrix 1:");
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                System.out.print(arr1[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Matrix 2:");
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                System.out.print(arr2[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                arr3[i][j] = arr1[i][j] - arr2[i][j]; // finds the difference of the elements of both the arrays
+            }
+        }
+
+        System.out.println("Difference of matrices:");
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                System.out.print(arr3[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        matDiff obj = new matDiff();
+        obj.acc();
+        obj.calc_dis();
+        obj.sc.close();
+    }
+}
